5448157f4b5166218f82aacc5ee81f6a9ba56bcf
Tom Dymel <td@wps.de>
2021-06-12 10:05:12 +0200

    Test module nodes get_fully_qualified_path

--- crates/arch_test_core/src/parser/tests/module_node.rs
+++ crates/arch_test_core/src/parser/tests/module_node.rs
@@ -28,3 +28,17 @@ fn included_nodes() {
     assert_eq!(tree[2].included_nodes(&tree), vec![3]);
     assert_eq!(tree[3].included_nodes(&tree), vec![]);
 }
+
+#[test]
+fn get_fully_qualified_path() {
+    let node1 = ModuleNode::new("WAMBO".to_owned(), 0, None, "WAMBO".to_owned());
+    let node2 = ModuleNode::new("WAMBO1".to_owned(), 1, Some(0), "WAMBO1".to_owned());
+    let node3 = ModuleNode::new("WAMBO2".to_owned(), 2, Some(1), "WAMBO2".to_owned());
+    let node4 = ModuleNode::new("WAMBO3".to_owned(), 3, Some(2), "WAMBO3".to_owned());
+    let tree: Vec<ModuleNode> = vec![node1, node2, node3, node4];
+
+    assert_eq!(tree[0].get_fully_qualified_path(&tree), "WAMBO".to_owned());
+    assert_eq!(tree[1].get_fully_qualified_path(&tree), "WAMBO::WAMBO1".to_owned());
+    assert_eq!(tree[2].get_fully_qualified_path(&tree), "WAMBO::WAMBO1::WAMBO2".to_owned());
+    assert_eq!(tree[3].get_fully_qualified_path(&tree), "WAMBO::WAMBO1::WAMBO2::WAMBO3".to_owned());
+}
