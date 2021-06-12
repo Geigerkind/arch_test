abf2ffc8f039b28555c2dd253ba073cb8e9537fc
Tom Dymel <td@wps.de>
2021-06-12 09:48:23 +0200

    Test possible uses

--- crates/arch_test_core/src/parser/tests/module_tree/mod.rs
+++ crates/arch_test_core/src/parser/tests/module_tree/mod.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+
 use crate::ModuleTree;
+use crate::parser::domain_values::ObjectUse;
 use crate::parser::entities::ModuleNode;
 
 #[test]
 fn correct_fully_qualified_names() {
+    // This also tests "correct_republish_paths"
     let module_tree = ModuleTree::new("src/parser/tests/module_tree/correct_fully_qualified_names/main.rs");
     let tree: &Vec<ModuleNode> = module_tree.tree();
 
@@ -20,4 +24,16 @@ fn correct_fully_qualified_names() {
 
     assert_eq!(tree[2].usable_objects[0].object_name, "WAMBO".to_owned());
     assert_eq!(tree[3].usable_objects[0].object_name, "TESTO".to_owned());
+}
+
+#[test]
+fn construct_possible_use_map() {
+    let module_tree = ModuleTree::new("src/parser/tests/module_tree/correct_fully_qualified_names/main.rs");
+    let use_map: &HashMap<String, ObjectUse> = module_tree.possible_uses();
+
+    assert_eq!(use_map.len(), 4);
+    assert!(use_map.contains_key("crate::republish::testo::TESTO"));
+    assert!(use_map.contains_key("crate::republish::wambo::WAMBO"));
+    assert!(use_map.contains_key("crate::test"));
+    assert!(use_map.contains_key("crate::republish::test"));
 }
