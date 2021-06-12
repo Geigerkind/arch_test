d71e1e91e8c220c0c206c5c4df592a23d6c4676d
Tom Dymel <td@wps.de>
2021-06-12 08:09:35 +0200

    Test impl trait

---
+++ crates/arch_test_core/src/parser/tests/parser/traits/impl_trait.rs
@@ -0,0 +1,5 @@
+impl TraitName for Bla {
+    fn test(a: b) -> c {
+
+    }
+}
--- crates/arch_test_core/src/parser/tests/parser/traits/mod.rs
+++ crates/arch_test_core/src/parser/tests/parser/traits/mod.rs
@@ -33,3 +33,18 @@ fn impl_normal() {
     assert_eq!(node_tree[0].usable_objects[1].object_name, "b".to_owned());
     assert_eq!(node_tree[0].usable_objects[2].object_name, "c".to_owned());
 }
+
+#[test]
+fn impl_trait() {
+    let mut node_tree = Vec::new();
+    let path = Path::new("src/parser/tests/parser/traits/impl_trait.rs");
+    parse_main_or_mod_file_into_tree(&mut node_tree, path, 0, None, "WAMBO".to_owned());
+
+    for i in 0..4 {
+        assert_eq!(node_tree[0].usable_objects[i].object_type(), &ObjectType::ImplicitUse);
+    }
+    assert_eq!(node_tree[0].usable_objects[0].object_name, "TraitName".to_owned());
+    assert_eq!(node_tree[0].usable_objects[1].object_name, "Bla".to_owned());
+    assert_eq!(node_tree[0].usable_objects[2].object_name, "b".to_owned());
+    assert_eq!(node_tree[0].usable_objects[3].object_name, "c".to_owned());
+}
