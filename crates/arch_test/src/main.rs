6039ea71f672619262e06de12b8a1daa134702ac
Tom Dymel <td@wps.de>
2021-06-13 11:02:09 +0200

    Add architecture json for core and adding exit code to main

--- crates/arch_test/src/main.rs
+++ crates/arch_test/src/main.rs
@@ -23,11 +23,14 @@ fn main() {
         let module_tree = ModuleTree::new(opts.input.to_str().unwrap());
         if let Err(err) = architecture.validate_access_rules() {
             err.print(&module_tree.tree());
+            std::process::exit(1);
         } else if let Err(err) = architecture.check_access_rules(&module_tree) {
             err.print(&module_tree.tree());
+            std::process::exit(1);
         } else if opts.check_for_complete_layer_specification {
             if let Err(err) = architecture.check_complete_layer_specification(&module_tree) {
                 err.print(&module_tree.tree());
+                std::process::exit(1);
             }
         }
     }
